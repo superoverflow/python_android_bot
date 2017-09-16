76ba43475497847906537c18e1a61070b69332f5
cyrustang <chiuyat88@gmail.com>
2017-09-16 22:36:44 +0100

    char card test generating cases from meta.txt

--- test_charcard.py
+++ test_charcard.py
@@ -1,33 +1,48 @@
+import logging
 import unittest
-from os import listdir, path
+from os import path
 
 from charcard import CharCard
 
 TEST_DATA_DIR = "test_input/char_cards/"
 
 
-class CharCardTest(unittest.TestCase):
-    def read_meta(self):
-        metafile = path.join(TEST_DATA_DIR, "meta.txt")
+def read_meta():
+    metafile = path.join(TEST_DATA_DIR, "meta.txt")
 
-        with open(metafile) as f:
-            lines_with_header = f.readlines()
-            lines = lines_with_header[1:]
-            result = [tuple(l.strip('\s\n').split(",")) for l in lines]
-            result = [(r[0], int(r[1]), int(r[2])) for r in result]
-        return result
+    with open(metafile) as f:
+        lines_with_header = f.readlines()
+        lines = lines_with_header[1:]
+        result = [tuple(l.strip('\s\n').split(",")) for l in lines]
+        result = [(r[0], int(r[1]), int(r[2])) for r in result]
+    return result
 
-    def search_char_chars(self):
-        return [f for f in listdir(TEST_DATA_DIR) if f.endswith('.png')]
 
+def make_test_func(desc, a, b):
     def test(self):
-        cases = self.read_meta()
+        self.assertEqual(a, b, desc)
 
-        for c in cases:
-            file = path.join(TEST_DATA_DIR, c[0])
-            char = CharCard(file)
-            print("%s, %s, %s" % (c, char.is_four_stars(), char.is_lv_40()))
+    return test
 
+
+class CharCardTest(unittest.TestCase):
+    longMessage = True
 
 if __name__ == '__main__':
+    FORMAT = "%(asctime)-15s [%(levelname)-6s] %(filename)s:%(lineno)3d  %(message)s"
+    logging.basicConfig(format=FORMAT, level=20)
+
+    meta = read_meta()
+
+    char_cards = {c[0]: CharCard(TEST_DATA_DIR + c[0]) for c in meta}
+    char_stars = {c[0]: c[1] for c in meta}
+    char_lv = {c[0]: c[2] for c in meta}
+
+    for k, v in char_cards.iteritems():
+        test_func_star = make_test_func(k, char_stars[k] == 4, v.is_four_stars())
+        test_func_lv = make_test_func(k, char_lv[k] == 40, v.is_lv_40())
+        test_name = k.strip('.png')
+        setattr(CharCardTest, 'test_stars_{0}'.format(test_name), test_func_star)
+        setattr(CharCardTest, 'test_lv_{0}'.format(test_name), test_func_lv)
+
     unittest.main()
